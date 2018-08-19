a624b884cee2339362e71d47a63116f361204ef4
beckhan522 <beckhan522@yahoo.co.jp>
2018-08-19 13:24:55 +0900

    Update btcrpc.py

--- btcrpc.py
+++ btcrpc.py
@@ -1,6 +1,6 @@
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+import bitcoin
+import bitcoin.rpc
 
-# rpc_user and rpc_password are set in the bitcoin.conf file
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332" % (rpc_user, rpc_password))
-best_block_hash = rpc_connection.getbestblockhash()
-print(rpc_connection.getblock(best_block_hash))
+bitcoin.SelectParams('testnet')
+proxy_connection = bitcoin.rpc.Proxy()
+print(proxy_connection.getinfo())
